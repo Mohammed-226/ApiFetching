eca8f19424e1d96c3af0a4ac2b5cf9c2ad75fbce
Mohammed-226 <94556189+Mohammed-226@users.noreply.github.com>
2022-01-17 11:50:44 +0530

    Update FetchApi.jsx

--- FetchApi.jsx
+++ FetchApi.jsx
@@ -20,13 +20,14 @@ const FetchApi = () => {
     };
     fetchComments();
   }, []);
+  // let lastPage = pageCount[pageCount.length - 1];
   let usersPerPage = 10;
   let pagesVisited = pageNumber * usersPerPage;
   let displayUser = state
     .slice(pagesVisited, pagesVisited + usersPerPage)
     .map(x => {
       return (
-        <tr className="bg-gray-200">
+        <tr className="bg-gray-400 text-center">
           <td className="border-2 border-black">{x.id}</td>
           <td className="border-2 border-black ">{x.title}</td>
           <td className="border-2 border-black">
@@ -40,7 +41,7 @@ const FetchApi = () => {
   let changePage = ({ selected }) => {
     setPageNumber(selected);
   };
-let initialPage = ({ selected }) => {
+  let initialPage = ({ selected }) => {
     if ((selected = 0.01)) {
       return setPageNumber(selected);
     }
@@ -54,8 +55,9 @@ let initialPage = ({ selected }) => {
 
   return (
     <Fragment>
-      <table className="border border-black border-collapse ml-[390px] mt-20">
-        <thead className=" bg-yellow-200">
+      <h1 className="ml-[45%] mt-10 text-2xl relative">FETCH API</h1>
+      <table className="border border-black border-collapse ml-[390px] mt-10">
+        <thead className=" bg-yellow-500">
           <th className="border-2 border-black ">Id</th>
           <th className="border-2 border-black ">Title</th>
           <th className="border-2 border-black ">Complted</th>
@@ -68,18 +70,18 @@ let initialPage = ({ selected }) => {
           nextLable={"Next"}
           pageCount={pageCount}
           onPageChange={changePage}
-          className="flex ml-[600px] bg-slate-300 w-[260px] mt-7 rounded"
+          className="flex ml-[560px]  w-[260px] mt-7 space-x-3"
         />
       </div>
- <div className="absolute ml-[700px]">
+      <div className="absolute ml-[700px]">
         <button
-          className="relative bg-slate-500 rounded w-[60px]"
+          className="relative bg-slate-500 rounded w-[60px] border-2"
           onClick={initialPage}
         >
           First
         </button>
         <button
-          className="relative ml-4 bg-slate-500 rounded w-[60px]"
+          className="relative ml-4 bg-slate-500 rounded w-[60px] border-2"
           onClick={finalPage}
         >
           Last
@@ -90,3 +92,4 @@ let initialPage = ({ selected }) => {
 };
 
 export default FetchApi;
+
